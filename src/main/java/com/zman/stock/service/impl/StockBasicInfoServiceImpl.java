df146195d12b4a0dd9fba79be3065251162f8c72
zman <vsmingren@qq.com>
2016-04-03 21:13:24 +0800

    no changes

--- src/main/java/com/zman/stock/service/impl/StockBasicInfoServiceImpl.java
+++ src/main/java/com/zman/stock/service/impl/StockBasicInfoServiceImpl.java
@@ -1,7 +1,12 @@
 package com.zman.stock.service.impl;
 
-import com.zman.stock.downloader.StockBasicInfoDownloader;
+import com.zman.stock.service.StockBasicInfoService;
 
-public class StockBasicInfoServiceImpl extends StockBasicInfoDownloader {
+public class StockBasicInfoServiceImpl implements StockBasicInfoService {
+
+    @Override
+    public void run() {
+
+    }
 
 }
